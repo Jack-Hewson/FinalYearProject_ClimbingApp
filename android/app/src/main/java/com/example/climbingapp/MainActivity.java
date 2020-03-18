c54d76348f01a06fc806f697036287fdbfd51613
Jack-Hewson <57640969+Jack-Hewson@users.noreply.github.com>
2020-03-18 16:12:48 +0000

    layout changes
    camera fragment was peventing chagnes via navigation bar. Framelayout added as part of relativelayout

--- android/app/src/main/java/com/example/climbingapp/MainActivity.java
+++ android/app/src/main/java/com/example/climbingapp/MainActivity.java
@@ -33,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+   //     Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+       FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
