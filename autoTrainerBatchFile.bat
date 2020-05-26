99643a4d3691bf8e31554a9905982dc372f20f24
Jack-Hewson <57640969+Jack-Hewson@users.noreply.github.com>
2020-05-26 02:22:57 +0100

    batch file added

---
+++ autoTrainerBatchFile.bat
@@ -0,0 +1,5 @@
+call C:\Users\jacks\Anaconda3\Scripts\activate.bat
+call conda activate tensorflow1
+cd C:\Users\jacks\OneDrive - University of Plymouth\_Fourth Year\PRCO304\tensorflow1\models\research\object_detection
+python firebaseStorageDownloader.py
+PAUSE
