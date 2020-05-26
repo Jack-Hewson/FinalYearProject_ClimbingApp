8113c22442f594f42f2c9b0066b0c03d487688f7
Jack-Hewson <57640969+Jack-Hewson@users.noreply.github.com>
2020-05-26 15:28:28 +0100

    Update resizer.py

--- tensorflow/models/research/object_detection/resizer.py
+++ tensorflow/models/research/object_detection/resizer.py
@@ -15,7 +15,8 @@ dir_path = os.getcwd()
 
 for filename in os.listdir(dir_path):
     # If the images are not .JPG images, change the line below to match the image type.
-    if filename.endswith(".JPG"):
+    if filename.endswith(".jpg"):
+        print(filename)
         image = cv2.imread(filename)
         resized = cv2.resize(image,None,fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
         cv2.imwrite(filename,resized)
